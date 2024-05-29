b74fa58f53478bc08a4536b9c9531ef9fdb72f9d
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-29 10:40:06 +0200

    Esercizio corretto
    Grazie per il suggerimento. Non so perché cerco sempre la via più difficile...

--- selfwork-cicli-1/main.js
+++ selfwork-cicli-1/main.js
@@ -1,10 +1,10 @@
 // Creare tabellina del 2 
 let num = 2;
-let iter = num*10;
 
-for(let i = num; i <= iter; i+=num){
 
-    console.log(i)
+for(let i = 1; i <= 10; i++){
+
+    console.log(i * num);
     
 
 }
