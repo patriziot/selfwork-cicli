9b6e6bb268436c8f61ee86a68337cbf6f680f627
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-29 13:15:56 +0200

    Ok tutto chiaro ora.

--- selfwork-cicli-2/main.js
+++ selfwork-cicli-2/main.js
@@ -1,7 +1,17 @@
 
+let accumulator = 0;
+let counter = 0;
+
 for (let i = 1; i <= 20; i++) {
     if (i % 2 == 0) {
-      console.log( i )}
+      console.log( i );
+    }else{
+        accumulator = accumulator + i
+        counter ++
+    }
     
   }
+  console.log(`accumulator = ${accumulator}`);
+  console.log(`counter = ${counter}`)
+  console.log(`risultato media = ${accumulator/counter}`)
 
